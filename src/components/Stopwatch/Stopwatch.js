42bdf7a36481caae1fc69a35975327ef1a0b85ee
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-02-22 23:51:13 +0100

    add the Stopwatch component

---
+++ src/components/Stopwatch/Stopwatch.js
@@ -0,0 +1,40 @@
+import Button from '../Button/Button';
+import Container from '../Container/Container';
+import FormattingTime from '../FormattingTime/FormattingTime';
+import { useEffect, useState } from 'react'; 
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const startTimer = () => {
+    setTimer(setInterval(() => {
+      setTime(prevValue => prevValue + 9);
+    }, 9))
+  };
+
+  const stopTimer = () => {
+    clearInterval(timer);
+  }
+  
+  const resetTimer = () => {
+    stopTimer();
+    setTime(0);
+  }
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  
+  return (
+    <Container>
+      <Button onClick={startTimer}>Start</Button>
+      <Button onClick={stopTimer}>Stop</Button>
+      <Button onClick={resetTimer}>reset</Button>
+    </Container>
+  );
+}
+
+export default Stopwatch;
